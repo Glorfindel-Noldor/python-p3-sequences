a80a6c6961c4a4445d618eb926f2791b8342fad4
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-07 16:56:42 -0500

    sorting a sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if(length == 0):
+        return print([])
+    elif(length == 1):
+        return print([0])
+    elif(length == 2):
+        return print([0,1])
+    elif length >= 10:
+        fibonacci_sequence = [0, 1] 
+        for i in range(2, length):
+            next_fibonacci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_fibonacci)
+
+            if len(fibonacci_sequence) == length:
+                return print(fibonacci_sequence)
+        
+        return print(fibonacci_sequence)
+
+    
